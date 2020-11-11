e073ec370880361a74a253006bef0963f4088b83
guytwem <guytwem@gmail.com>
2020-11-12 10:11:13 +1100

    game mechanics

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5;
+    
+
+
+    private bool facingRight;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        
+
+        float _horizontal = Input.GetAxis("Horizontal");
+        Movement(_horizontal);
+        Flip(_horizontal);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Movement(float _horizontal)
+    {
+        rb.velocity = new Vector2(_horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip(float _horizontal)
+    {
+        if (_horizontal > 0 && !facingRight || _horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public Text score;
+    private float currentScore;
+
+
+    
+    private void Update()
+    {
+        
+
+        if (player.transform.position.y > currentScore)
+        {
+            currentScore = player.transform.position.y;
+            
+        }
+
+        score.text = "Score: " + currentScore.ToString();
+    }
+}
